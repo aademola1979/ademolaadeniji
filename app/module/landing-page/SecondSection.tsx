a777a16c9c5a93d14ed53b674815880210f159f2
ademola adeniji <aademola750@gmail.com>
2024-08-06 23:04:22 +0100

    added

--- app/module/landing-page/SecondSection.tsx
+++ app/module/landing-page/SecondSection.tsx
@@ -8,7 +8,7 @@ import Offers from "./Offers"
 
 const SecondSection = () => {
     return (
-        <div className="flex flex-col gap-[4rem] py-[4rem] !max-h-fit">
+        <div className="flex flex-col gap-[4rem] py-0 lg:py-[4rem] !max-h-fit">
             <DesktopOnly />
             <Offers />
         </div>
