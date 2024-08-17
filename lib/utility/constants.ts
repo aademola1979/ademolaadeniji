a599375bb641d214c442acb65ad4a98c049c7fc1
ademola adeniji <aademola750@gmail.com>
2024-08-17 23:22:44 +0100

    constant edited

--- lib/utility/constants.ts
+++ lib/utility/constants.ts
@@ -2,7 +2,7 @@ export const workData = [
     {
         id:"work-1",
         title:"LitHub9ja",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        description:"LitHub9ja, a blog site built to be easily navigated.",
         imgUrl:"/images/lithub.png",
         category:"blog",
         siteUrl:"https://lithub9ja.vercel.app",
@@ -12,10 +12,10 @@ export const workData = [
     {
          id:"work-2",
         title:"Africare",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        description:"Built to make health care accessible to many people across Africa.",
         imgUrl:"/images/africare.png",
         category:"Telemedicine",
-        siteUrl:"https://nollywoodpassion.render.com",
+        siteUrl:"https://africare-frontend.vercel.app/",
         
     },
 ]
