934902987a1216c1904b210e6533ae9fd7ababed
ademola adeniji <aademola750@gmail.com>
2024-08-17 23:05:11 +0100

    edited projec card

--- app/module/projects/WorkCard.jsx
+++ app/module/projects/WorkCard.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { fadeIn } from "@/lib/motion";
 import { motion } from "framer-motion";
-import Image from "next/image";
+
 
 const WorkCard = ({ workCardProp, setActive }) => {
   const { work, index, active, } = workCardProp;
@@ -10,26 +10,29 @@ const WorkCard = ({ workCardProp, setActive }) => {
   return (
     <motion.div
       variants={fadeIn('right', 'spring', index * 0.25, 0.75)}
-      className={`relative ${active === id ? 'active-work-card' : 'inactive-work-card'} project-card ease-out-flex`}
+      className={`relative ${active === id ? 'active-work-card' : 'inactive-work-card'} project-card ease-out-flex` }
       onClick={() => setActive(id)}>
-      <Image src={imgUrl} alt={title} height={700} width={160}
+       <img src={imgUrl} alt={title} height={700} width={160}
         className="project-img" />
       {active !== id
         ? (<h3 className="inactive-h3">
           {title}
         </h3>
         )
-        : (<div className="active-div min-w-full max-w-full max-h-full overflow-y-hidden">
-          <div className="flex justify-between">
+        : (<div className="active-div min-w-full max-w-full max-h-full overflow-hidden">
+          <div className="flex justify-between px-2">
+
             <div className="text-start project-category h-[60px] py-1 px-4 text-white rounded-3xl glassmorphism mb-[4rem]">
               {category}
             </div>
-            <a href={siteUrl} target="_blank" className="text-base font-medium text-white">
-              <Button className="text-base font-medium text-white">Visit Site</Button>
+            <a href={siteUrl} target="_blank" className="text-base font-medium text-white flex justify-center">
+              <Button variant="link" className="text-base font-medium text-white">Visit Site</Button>
             </a>
           </div>
-          <p className="active-p">{description}</p>
+         <p className="active-p">{description}</p>
+
         </div>
+
         )
 
       }
--- lib/utility/constants.ts
+++ lib/utility/constants.ts
@@ -18,32 +18,4 @@ export const workData = [
         siteUrl:"https://nollywoodpassion.render.com",
         
     },
-    {
-        id:"work-3",
-        title:"LitHub9ja",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        imgUrl:"/images/lithub.png",
-        category:"blog",
-        siteUrl:"https://lithub9ja.vercel.app",
-
-    },
-
-    {
-         id:"work-4",
-        title:"Africare",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        imgUrl:"/images/africare.png",
-        category:"Telemedicine",
-        siteUrl:"https://nollywoodpassion.render.com",
-        
-    },
-    {
-        id:"work-5",
-       title:"Africare",
-       description:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-       imgUrl:"/images/africare.png",
-       category:"Telemedicine",
-       siteUrl:"https://nollywoodpassion.render.com",
-       
-   }
 ]
